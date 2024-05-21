b2a4448064375848cfb545f1afd519919b21ec7e
bkb2135 <98138173+bkb2135@users.noreply.github.com>
2024-05-21 09:29:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,12 @@
 # Uninstalling mathgenerator
 pip uninstall mathgenerator -y
 
+# Install npm 
+apt update
+apt install npm -y
+
+# Install pm2 globally
+npm install -g pm2
+
 # Installing package from the current directory
 pip install -e .
